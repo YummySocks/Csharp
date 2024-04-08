f6be5d106847704e206385a2a2594ab9a0a6be43
Patrick Sutcliffe <patrickdsutcliffe@gmail.com>
2024-04-08 00:59:57 -0400

    more random stuffs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,5 @@
-﻿// Outer loop
-for (int i = 1; i <= 2; ++i) 
+﻿string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+foreach (string i in cars) 
 {
-  Console.WriteLine("Outer: " + i);  // Executes 2 times
-
-  // Inner loop
-  for (int j = 1; j <= 3; j++) 
-  {
-    Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
-  }
+  Console.WriteLine(i);
 }
